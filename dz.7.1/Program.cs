0e692a0ceef1f557a4c505b4dbe558add3325311
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2022-12-21 10:27:25 +0300

    Добавили решение первой задачи

--- dz.7.1/Program.cs
+++ dz.7.1/Program.cs
@@ -6,3 +6,38 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
+int rows = ReadInt("Сколько строк в Вашем массиве? ");
+int colums = ReadInt("Сколько столбцов в Вашем массиве? ");
+double[,] numbers = new double[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-250, 250) / 10.0;
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+ 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
